fd73453d92c14c21d3dea76abfa68df0e4936597
Martin Hochel <hochelmartin@gmail.com>
2017-03-09 10:28:42 +0100

    initial commit

---
+++ src/__tests__/app/calculator.spec.js
@@ -0,0 +1,16 @@
+import {add, divide, multiply, subtract} from '../../app/calculator';
+
+describe(`Calculator`, () => {
+
+  describe(`add`, () => {
+
+    it('adds 1 + 2 to equal 3', () => {
+      const expected = 3;
+      const actual = add(1, 2);
+
+      expect(actual).toBe(3);
+    });
+
+  });
+
+});
